610f60cfc8a3660d66687cb9b8fca0cd67570c9b
SafychCSS <safaraliev.renat@yandex.ru>
2020-12-04 16:16:35 +0300

    change checkbox

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -6,12 +6,10 @@
                 <div class="card-body">
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
-                            <div class="input-group-text todo-list__check-all">
-                                <div class="checkbox">
-                                    <input type="checkbox" id="checkall" class="visibility-hidden checkbox__input">
-                                    <label for="checkall" class="checkbox__label"></label>
-                                </div>
-                            </div>
+                            <label class="checkbox checkbox--check-all flex-grow-1 input-group-text">
+                                <input type="checkbox" class="visibility-hidden checkbox__input">
+                                <span class="checkbox__text">&nbsp;</span>
+                            </label>
                         </div>
                         <input type="text" v-model="newTodo" class="form-control form-control-lg" placeholder="What needs to be done?">
                         <button type="button" class="btn btn-success todo-list__add">Add</button>
@@ -19,12 +17,10 @@
                     <hr>
                     <div class="list-group">
                         <div class="todo-list__item list-group-item list-group-item-action d-flex align-items-center shadow-sm">
-                            <div class="checkbox flex-grow-1">
-                                <input type="checkbox" id="check" class="visibility-hidden checkbox__input">
-                                <label for="check" class="checkbox__label flex-grow-1">
-                                    <span class="checkbox__text">Cras justo odio</span>
-                                </label>
-                            </div>
+                            <label class="checkbox flex-grow-1">
+                                <input type="checkbox" class="visibility-hidden checkbox__input">
+                                <span class="checkbox__text">Cras justo odio</span>
+                            </label>
                             <button type="button" class="todo-list__item-remove close" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
@@ -42,7 +38,21 @@ export default {
     name: 'todo-list',
     data() {
         return {
-            newTodo: ''
+            newTodo: '',
+            todos: [
+                {
+                    id: 1,
+                    title: 'Learn vue',
+                    completed: false,
+                    prevValue: ''
+                },
+                {
+                    id: 2,
+                    title: 'Learn Laravel',
+                    completed: false,
+                    prevValue: ''
+                }
+            ]
         }
     }
 
